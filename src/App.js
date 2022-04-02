0b1792a9305e3b5e0b5b279d87a52c1a08402b1b
yaroslav-re <repinyar7@mail.ru>
2022-04-02 11:11:30 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Note from "./components/Note";
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log(event.target, "event.target");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log("handler", event.target.value);
+    setNewNote(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
-}
-
+};
 export default App;
+
+// остановились на controled component
